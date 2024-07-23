6d0794d74d50115144bb8b7b875d870d12b26542
kavy009 <chauhankavya9116@gmail.com>
2024-07-23 11:09:36 +0530

    Add files via upload

---
+++ Kavya01.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+void main()
+{     int length=50,breadth=30,perimeter,area;
+
+        perimeter=2*(length+breadth);
+        area=(length*breadth);
+        printf("The area is %d and the perimeter is %d by given data",area,perimeter);
+
+}
+
---
+++ Kavya02.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main(){
+
+      int p=50000,t=3,si;
+      float r=5.5;
+
+      si=(p*r*t)/100;
+
+      printf("\n The Simple Interest is %d",si);
+
+
+
+
+
+}
