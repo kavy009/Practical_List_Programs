b7532d3b8e701df0d7d9d266f0d624357596ed56
kavy009 <chauhankavya9116@gmail.com>
2024-08-01 14:30:44 +0530

    Add files via upload

---
+++ Kavya08.c
@@ -0,0 +1,18 @@
+ #include <stdio.h>
+
+ void main()
+ {
+     int marks;
+     printf("Enter Your Marks:");
+     scanf("%d",&marks);
+
+     (marks>=90 && marks<=100) ? printf("Your Grade is A"):
+     (marks>=80 && marks<90) ? printf("Your Grade is B"):
+     (marks>=70 && marks<80) ? printf("Your Grade is C"):
+     (marks>=60 && marks<70) ? printf("Your Grade is D"):
+     (marks>=50 && marks<60) ? printf("Your Grade is F"):
+         printf("Sorry you are Failed");
+
+
+
+ }
