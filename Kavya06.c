11f75f9462e3e3c2d5cbd4b3f66a30bd5b6ddcbd
kavy009 <chauhankavya9116@gmail.com>
2024-08-01 14:32:46 +0530

    Add files via upload

---
+++ Kavya06.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main()
+
+{
+    float Ramanbungalow=12000000.0,Ramanplot=6000000.0,Ramancar=3000000.0;
+
+    float Sumanapartment=11000000.0,Sumanhotel=8000000.0,Sumancar=8000000.0;
+
+    float Ramantotalwealth=Ramanbungalow+Ramanplot+Ramancar;
+    float Sumantotalwealth=Sumanapartment+Sumanhotel+Sumancar;
+
+    if (Ramantotalwealth>Sumantotalwealth)
+    { printf("Raman possesses greater wealth than Suman\n");
+    } else if(Sumantotalwealth>Ramantotalwealth)
+    {
+        printf("Suman possesses greater wealth than Raman\n");
+    }
+        else {
+            printf("Raman and Suman possesses equal wealth");
+        }
+    }
+
+
