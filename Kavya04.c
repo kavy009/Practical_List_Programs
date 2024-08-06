5e55aeed98185ea5f87c6e1f61a6c359ec6455c1
kavy009 <chauhankavya9116@gmail.com>
2024-08-06 10:24:21 +0530

    Add files via upload

---
+++ Kavya04.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+    int c,f;
+
+
+
+    printf("Enter the value of temperature in Celcius:");
+    scanf("%d",&c);
+    f=9*c/5 +32;
+
+    printf("Your answer in Fahrenheit is %d ",f);
+
+}
