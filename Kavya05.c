5dfc989753b84bc13f80604b0dd3b15f2ed1acf8
kavy009 <chauhankavya9116@gmail.com>
2024-11-14 19:09:32 +0530

    Kavya05.c

--- Kavya05.c
+++ Kavya05.c
@@ -1,3 +1,4 @@
+// This Program is prepared by 24CE017 Kavya
 #include <stdio.h>
 void main()
 {
@@ -6,26 +7,26 @@ void main()
     scanf("%d",&a);
     printf("Enter the value of Second Number:");
     scanf("%d",&b);
-    printf("First Choice is +");
-    printf("\nSecond Choice is -");
-    printf("\nThird Choice is *");
-    printf("\nFourth Choice is /");
+    printf("Enter 1 for Addition");
+    printf("\nEnter 2 for Subtraction");
+    printf("\nEnter 3 for Multipilcation");
+    printf("\nEnter 4 for Division/");
     printf("\nEnter Your Choice:");
     scanf("%d",&ch);
 
 
     switch(ch)
     {
-        case 1 : printf("\n Addition of given two number is %d ",a+b);
+        case 1 : printf("\n Addition of given two numbers is %d ",a+b);
         break;
 
-        case 2 : printf("\n Subtraction of given two number is %d ",a-b);
+        case 2 : printf("\n Subtraction of given two numbers is %d ",a-b);
         break;
 
-        case 3 : printf("\n Multiplication of given two number is %d ",a*b);
+        case 3 : printf("\n Multiplication of given two numbers is %d ",a*b);
         break;
 
-        case 4 : printf("\n Division of given two number is %d ",a/b );
+        case 4 : printf("\n Division of given two numbers is %d ",a/b );
         break;
 
         default: printf("\n Invalid Input");
@@ -33,6 +34,9 @@ void main()
 
 
     }
+
+    printf("\n\n24Ce017_Kavya");
+
 
 
 
