ed683ca9dcb3be4a37a79e12b66918281295ec20
kavy009 <chauhankavya9116@gmail.com>
2024-11-14 18:34:42 +0530

    Kavya03.c

--- Kavya03.c
+++ Kavya03.c
@@ -1,33 +1,30 @@
 
 #include<stdio.h>
 
-void main(){
+void main()
 
-     int gp=1441981744,man,woman;
-     float tlrM,tlrW,totalilliterateMan,totalilliterateWoman;
+{
 
-     man=gp*0.516;
-     woman=gp*0.484;
+     int TotalPopulation=1441981744,man,woman;
+     float LiterateMan,LiterateWoman,totalilliterateMan,totalilliterateWoman;
 
-     tlrM=0.8095*man;
-     tlrW=0.6284*woman;
+     man=TotalPopulation*0.516;
+     woman=TotalPopulation*0.484;
 
-     totalilliterateMan=man-tlrM;
-     totalilliterateWoman=woman-tlrW;
+     LiterateMan=0.8095*man;
+     LiterateWoman=0.6284*woman;
+
+     totalilliterateMan=man-LiterateMan;
+     totalilliterateWoman=woman-LiterateWoman;
 
     printf("\n Total number of man is %d in bharat population for year 2024.\n",man);
     printf("\n Total number of woman is %d in bharat population for year 2024.\n",woman);
 
-    printf("\n Total Literacy Number of Man is %.0f.\n",tlrM);
-    printf("\n Total Literacy Number of Woman is %.0f.\n",tlrW);
+    printf("\n Total Literacy Number of Man is %.0f.\n",LiterateMan);
+    printf("\n Total Literacy Number of Woman is %.0f.\n",LiterateWoman);
 
 
     printf("\n Total illiterate Number of Man is %0.f\n",totalilliterateMan);
     printf("\n Total illiterate Number of Woman is %0.f\n",totalilliterateWoman);
-
-
-
-
-
 
 }
